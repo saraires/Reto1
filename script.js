74d7e4881ce787a18bee1deb781c6dc113dcb634
saraires <sarairestrepo22@gmail.com>
2020-12-14 17:40:09 -0500

    prueba reto 2

---
+++ script.js
@@ -0,0 +1,20 @@
+var hojas = "a";
+var i = 0;
+document.querySelector('h2').innerHTML = arbolito();
+
+Math.random[0, 1];
+
+function arbolito() {
+
+    for (let step = 1; step <= 6; step++) {
+        while (i <= 15) {
+            //console.log (hojas);
+            for (let s = i; s <= i; s++)
+                hojas = Math.round(Math.random());
+        }
+        i += 3;
+    }
+    return hojas
+}
+
+
